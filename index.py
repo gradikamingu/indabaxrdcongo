59b46b6e01360d37490fa0e1085673bcc8954de4
Gradi Kamingu Lubwele <gradi.l.kmingu@aims-senegal.org>
2025-08-31 02:11:12 +0100

    Un commit test.

---
+++ index.py
@@ -0,0 +1,11 @@
+# Programme pour additionner deux nombres
+
+# On demande à l'utilisateur d'entrer deux nombres
+a = float(input("Entrez le premier nombre : "))
+b = float(input("Entrez le deuxième nombre : "))
+
+# On calcule la somme
+somme = a + b
+
+# On affiche le résultat
+print("La somme de", a, "et", b, "est :", somme)
