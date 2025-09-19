13152c9cf63794e9c8f83b587596b072f4afd509
Gradi Kamingu Lubwele <gradi.l.kmingu@aims-senegal.org>
2025-09-19 16:07:17 +0100

    Update index.py
    Ajout de la différence et du produit.

--- index.py
+++ index.py
@@ -7,5 +7,13 @@ b = float(input("Entrez le deuxième nombre : "))
 # On calcule la somme
 somme = a + b
 
+# On calcule la différence
+difference = a - b
+
+# On calcule le produit
+produit = a * b
+
 # On affiche le résultat
 print("La somme de", a, "et", b, "est :", somme)
+print("La différence de", a, "et", b, "est :", difference)
+print("Le produit de", a, "et", b, "est :", produit)
